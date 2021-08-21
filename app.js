f16fd642babc77f6f36922d207bcca826826a2d4
bjrodriguemo <brayanjulianrodriguezmoreno23@gmail.com>
2021-08-20 20:41:55 -0500

    Save version  0001

---
+++ app.js
@@ -0,0 +1,21 @@
+// Tutorial Youtube - Video 1
+// https://youtu.be/mG4U9t5nWG8
+
+// Ver version de NodeJS => node -v
+
+// Crear archivo app.js
+const frutas = ['platano','manzana','platano','pera'];
+
+// Recorrer arreglo
+frutas.forEach(item => {
+    console.log(item); // Imprimir en consola
+});
+// Ejecutar en consola => node app
+/*
+IMPRESION EN CONSOLA:
+platano
+manzana
+platano
+pera
+
+*/
