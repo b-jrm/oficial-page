a8ffa1b7e9916d6d82e323ac028f935a953049b2
bjrodriguemo <brayanjulianrodriguezmoreno23@gmail.com>
2021-08-21 23:01:37 -0500

    Video 05  - Intro to express

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-const http = require('http');
-
-const server = http.createServer((req, res) => {
-    res.end(`Response with port ${port} ...`);
-});
-
+const express = require('express');
+const app = express();
 const port = 3000;
 
-server.listen(port,() => {
-    console.log(`Listener requests with port ${port} ...`);
-});
+app.use(express.static(__dirname+"/public")); // Carpeta estatica public
+
+// Routers
+app.get('/profile',(req,res) => { res.send(`This page profile with express ...`) })
+
+app.use((req,res) => { res.status(404).sendFile(__dirname+"/public/404.html") })
+
+// Listener
+app.listen(port,() => { console.log(`Listener requests with port ${port} ...`) })
+
+
---
+++ video04.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.end(`Response with port ${port} ...`);
+});
+
+const port = 3000;
+
+server.listen(port,() => {
+    console.log(`Listener requests with port ${port} ...`);
+});
