2079541d236689be3d3f61060581d774fdd3278b
bjrodriguemo <brayanjulianrodriguezmoreno23@gmail.com>
2021-08-21 21:35:04 -0500

    Video 03 - Tutorial youtube Bluuweb

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const {frutas,dinero} = require('./frutas');
 
+const cowsay = require('cowsay');
+
 frutas.forEach(item => {
     console.log(item); // Imprimir en consola
 });
 
 console.log(dinero);
+
+console.log(cowsay.say({
+    text: 'Hola Julian, Curso NodeJS',
+    e: 'Oo',
+    T: 'U'
+}));
 
